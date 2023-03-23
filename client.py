f51e50fbbc018634a887519db8bbcde754f7741b
anupamvamsi <anupam.ismav@gmail.com>
2023-03-23 11:31:31 +0530

    updated server and client

--- client.py
+++ client.py
@@ -1,25 +1,34 @@
 import socket
+import threading
 import sys
 
 HOST = "127.0.0.1"
-# HOST = "10.1.0.242"
-PORT = 23422
+# HOST = "10.1.0.78"
+PORT = 23425
 
-if len(sys.argv) == 2:
-    msg = '0' + sys.argv[1]
-elif len(sys.argv) > 2:
-    sys.argv[1] = '1' + sys.argv[1]
-    msg = sys.argv[1:]
-    msg = ' '.join(msg)
-else:
-    msg = "2Please specify a filename or a string of characters."
 
-msg = bytearray((msg), 'utf-8')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((HOST, PORT))
 
-    client.sendall(msg)
-    data = client.recv(1024)
+def sendDataToServer(data=''):
+    while True:
+        try:
+            data = client.recv(1024)
+            print(data.decode('utf-8'))
+            msg = input('Message or file name: ')
 
-print("Received from the server: ", data.decode())
+            if msg == 'exit' or msg == 'stop':
+                client.sendall(msg.encode('utf-8'))
+                client.close()
+                break
+
+            client.sendall(msg.encode('utf-8'))
+        except Exception as e:
+            print("ERROR:", e)
+            client.close()
+            break
+
+
+if __name__ == "__main__":
+    sendDataToServer()
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import json
 import socket
+import threading
 import os
 
 # HOST = "10.1.0.78"
 HOST = "127.0.0.1"
-PORT = 23422
+PORT = 23425
 
 
-def convertToJSON(text):
+def wordCounter(text):
     wordCount = {}
     for word in text.split():
         if wordCount.get(word, 0) == 0:
@@ -15,49 +16,84 @@ def convertToJSON(text):
         else:
             wordCount[word] += 1
 
-    json_data = bytearray(str(wordCount), 'utf-8')
-
-    json_data1 = json.dumps(wordCount, indent=4)
-    json_data1 = bytearray(str(json_data1), 'utf-8')
-
-    return json_data1
+    return wordCount
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+def convertToJSON(dictionary, indent=False):
+    jsonData = json.dumps(
+        dictionary, indent=4) if indent is True else json.dumps(dictionary)
+    jsonData = bytearray(str(jsonData), 'utf-8')
+
+    return jsonData
+
+
+def getText(converted):
+    fileName = converted
+    text = converted
+    try:
+        fd = os.open(fileName, os.O_RDWR)
+
+        print(f'Processing file "{fileName}"...')
+
+        readBytes = os.read(fd, 1024)
+        text = readBytes.decode()
+        return text
+
+    except:
+        print(f'Processing message sent by client...')
+        return text
+
+    return {'error': 'Error. Could not process text.'}
+
+
+def handleClient(clientSocket, clients):
+    clientNumber = clients.index(clientSocket) + 1
+    while True:
+        data = clientSocket.recv(1024)
+        msg = data.decode('utf-8')
+
+        if msg == 'exit' or msg == 'stop':
+            print(f'Client {clientNumber} has disconnected.')
+            break
+        if not data:
+            print("No more data.")
+            break
+        if data.decode('utf-8').isspace():
+            print(f'No data received from the client {clientNumber}.')
+            break
+
+        print(f"Data received from the client: {data}")
+        converted = data.decode()
+        text = getText(converted)
+
+        if type(text) == dict:
+            jsonData = convertToJSON(text)
+        else:
+            wordCount = wordCounter(text)
+            jsonData = convertToJSON(wordCount, True)
+
+        clientSocket.sendall(jsonData)
+        print(f'Done. Sent word count result to client {clientNumber}.')
+
+
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen()
 
+    clients = []
+
+    print(f'Server is running on {HOST}, on port {PORT}.')
+    count = 0
+
     while True:
-        clientSocket, addr = server.accept()
+        clientSocket, address = server.accept()
+        count += 1
+        clients.append(clientSocket)
 
-        with clientSocket:
-            print(f"Connected by {addr}")
+        print(f"Connected to client {count} by {address}")
+        clientSocket.sendall(convertToJSON({'message': 'Connected to server'}))
 
-            while (True):
-                data = clientSocket.recv(1024)
-                if not data:
-                    break
-
-                converted = data.decode()
-
-                fileName = ''
-                txt = ''
-                if converted[0] == '0':
-                    fileName = converted[1:]
-
-                    # returns file descriptor
-                    fd = os.open(fileName, os.O_RDWR)
-                    readBytes = os.read(fd, 1024)
-                    convertedBytes = readBytes.decode()
-
-                    json_data = convertToJSON(convertedBytes)
-
-                if converted[0] == '1':
-                    txt = converted[1:]
-                    data = bytearray(txt, 'utf-8')
-                    json_data = convertToJSON(txt)
-                if converted[0] == '2':
-                    data = bytearray(converted[1:], 'utf-8')
-                    json_data = data
-
-                clientSocket.sendall(json_data)
+        thread = threading.Thread(
+            target=handleClient, args=(clientSocket, clients,))
+        thread.start()
